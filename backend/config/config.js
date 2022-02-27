416bf6158b9d03ce33c37635201bfa30fe9aa1d2
zubair ali <zubairkamboh9010@gmail.com>
2022-02-27 16:27:55 +0500

    production prepare

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECTION)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log('Connected to db')
   } catch (err) {
     console.log('something not good')
